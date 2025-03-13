38872c7e5668489c958624f5a897804d09fdcb2a
Ruth <165259669+eelut@users.noreply.github.com>
2025-03-14 00:05:08 +0300

    Created 'Merging Arrays' by A2SV Hub

---
+++ 13-Mar-2025/Merging Arrays 306014.py
@@ -0,0 +1,20 @@
+# Problem: Merging Arrays - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
+
+n,m=map(int,input().split(" "))
+a=list(map(int,input().split(" ")))
+b=list(map(int,input().split(" ")))
+
+ans=[]
+i ,j =0,0
+while i<len(a) and j<len(b):
+    if a[i]<b[j]:
+        ans.append(a[i])
+        i +=1
+    else:
+        ans.append(b[j])
+        j +=1
+ans.extend(a[i:])
+ans.extend(b[j:]) 
+print (*ans)
+
+ 
