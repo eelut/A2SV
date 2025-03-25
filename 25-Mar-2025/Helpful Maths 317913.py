0a1e3f6d5b8789202158ca531b690dc53e6ad204
Ruth <165259669+eelut@users.noreply.github.com>
2025-03-25 18:39:54 +0300

    Created 'Helpful Maths' by A2SV Hub

---
+++ 25-Mar-2025/Helpful Maths 317913.py
@@ -0,0 +1,9 @@
+# Problem: Helpful Maths - https://codeforces.com/problemset/problem/339/A
+
+s=input()
+s_without_plus=s.replace("+","")
+s_int = list(map(int, s_without_plus))
+s_int.sort()
+s_str=[str(i) for i in s_int]
+s_complete="+".join(s_str)
+print(s_complete)
