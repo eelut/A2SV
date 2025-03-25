e9038fe67de07d323a3c6e1b19c49a8436ec3e27
Ruth <165259669+eelut@users.noreply.github.com>
2025-03-25 18:39:41 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 25-Mar-2025/Petya and Strings 317911.py
@@ -0,0 +1,11 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+first_string=input().lower()
+second_string=input().lower()
+
+if first_string == second_string:
+    print(0)
+elif first_string < second_string:
+    print(-1)
+elif first_string > second_string:
+    print(1)
