6d72c5f8206ad70da75572d0fcad8831313f095c
Ruth <165259669+eelut@users.noreply.github.com>
2025-03-31 01:55:13 +0300

    Created 'Power of Two' by A2SV Hub

---
+++ 30-Mar-2025/Power of Two 322359.py
@@ -0,0 +1,11 @@
+# Problem: Power of Two - https://leetcode.com/problems/power-of-two/
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and (n & (n - 1)) == 0
+
+        
