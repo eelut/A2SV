f94d2f749d8d8038706ea09e5e387e969e5b14c9
Ruth <165259669+eelut@users.noreply.github.com>
2025-04-02 00:48:12 +0300

    Created 'Continuous Subarray Sum' by A2SV Hub

---
+++ 01-Apr-2025/Continuous Subarray Sum 323682.py
@@ -0,0 +1,21 @@
+# Problem: Continuous Subarray Sum - https://leetcode.com/problems/continuous-subarray-sum/
+
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        remainder={0:-1}
+        total =0
+        for i,n in enumerate(nums):
+            total +=n
+            r= total %k
+            if r not in remainder:
+                remainder[r]=i
+            elif i-remainder[r] >1:
+                return True
+        return False
+
+             
