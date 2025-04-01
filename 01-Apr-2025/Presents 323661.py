b03692ceec10f7be3c110823b292049ece1e358d
Ruth <165259669+eelut@users.noreply.github.com>
2025-04-02 00:03:04 +0300

    Created 'Presents' by A2SV Hub

---
+++ 01-Apr-2025/Presents 323661.py
@@ -0,0 +1,9 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+n=int(input())
+p=list(map(int,input().split()))
+res=[] 
+for i in range (n):
+    index=p.index(i+1)
+    res.append(index +1)
+print (" ".join((map(str,res))))
