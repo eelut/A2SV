c1987a3e3bf5316707d47bd6ad2c3766b4a3aacc
Ruth <165259669+eelut@users.noreply.github.com>
2025-04-02 00:02:40 +0300

    Created 'Way Too Long Words' by A2SV Hub

---
+++ 01-Apr-2025/Way Too Long Words 323660.py
@@ -0,0 +1,11 @@
+# Problem: Way Too Long Words - https://codeforces.com/problemset/problem/71/A
+
+n = int(input())
+
+a = [input().strip() for _ in range(n)]
+
+for i in a:
+    if len(i) <= 10:
+        print(i)
+    else:
+        print(i[0]+str(len(i)-2)+i[-1])
