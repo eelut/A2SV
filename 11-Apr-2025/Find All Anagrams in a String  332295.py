33f9fc90d1abfbbb57fa13b96cb12e4d32d1489b
Ruth <165259669+eelut@users.noreply.github.com>
2025-04-11 22:55:32 +0300

    Created 'Find All Anagrams in a String ' by A2SV Hub

---
+++ 11-Apr-2025/Find All Anagrams in a String  332295.py
@@ -0,0 +1,19 @@
+# Problem: Find All Anagrams in a String  - https://leetcode.com/problems/find-all-anagrams-in-a-string/
+
+from collections import Counter
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        ans=[]
+        i=0
+        if len(p) > len(s):
+            return []
+        p_count=Counter(p)
+        for i in range(len(s)-len(p)+1):
+            s_count=Counter(s[i:i+len(p)])
+            if s_count==p_count:
+                ans.append(i)
+            
+        return ans
+
+
+        
