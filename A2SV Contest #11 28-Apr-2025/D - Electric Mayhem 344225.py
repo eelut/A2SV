451b62a48ce0d5b7f7e3e3549b9fef76958bf9ce
Ruth <165259669+eelut@users.noreply.github.com>
2025-04-28 21:28:35 +0300

    Created 'D - Electric Mayhem' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/D - Electric Mayhem 344225.py
@@ -0,0 +1,10 @@
+# Problem: D - Electric Mayhem - https://codeforces.com/gym/605795/problem/D
+
+s=input()
+stack=[]
+for i in range(len(s)):
+    if stack and stack[-1]==s[i]:
+        stack.pop()
+    else:
+        stack.append(s[i])
+print("Yes"if len(stack)==0 else "No")
