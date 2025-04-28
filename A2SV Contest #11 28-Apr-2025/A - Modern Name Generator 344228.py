729096bcce974ce475d9e4c8da472682fb51a27b
Ruth <165259669+eelut@users.noreply.github.com>
2025-04-28 21:28:53 +0300

    Created 'A - Modern Name Generator' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/A - Modern Name Generator 344228.py
@@ -0,0 +1,9 @@
+# Problem: A - Modern Name Generator - https://codeforces.com/gym/605795/problem/A
+
+t=int(input())
+for _ in range(t):
+    old_name=input()
+    words=old_name.split()
+    modern="".join(word[0]for word in words)
+    print(modern)
+                   
