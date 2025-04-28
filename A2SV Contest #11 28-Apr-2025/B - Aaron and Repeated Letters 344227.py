6505824d5de8bd3e416ae11f8e581564729a3371
Ruth <165259669+eelut@users.noreply.github.com>
2025-04-28 21:28:47 +0300

    Created 'B - Aaron and Repeated Letters' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/B - Aaron and Repeated Letters 344227.py
@@ -0,0 +1,10 @@
+# Problem: B - Aaron and Repeated Letters - https://codeforces.com/gym/605795/problem/B
+
+s=input()
+result = []
+for char in s:
+    if not result or char != result[-1]:
+        result.append(char)
+    else:
+        result.pop()
+print("".join(result)) 
