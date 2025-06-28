6bc7d632d26c9ac9f1ab4a8061079a3b7183d203
Ruth <165259669+eelut@users.noreply.github.com>
2025-06-29 00:50:37 +0300

    Created 'Employee Importance' by A2SV Hub

---
+++ 28-Jun-2025/Employee Importance 371524.py
@@ -0,0 +1,13 @@
+# Problem: Employee Importance - https://leetcode.com/problems/employee-importance/
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        def dfs(emp):
+            imp = emps[emp].importance            
+            for s in emps[emp].subordinates:
+                imp += dfs(s)
+            return imp
+        
+        emps= {emp.id: emp for emp in employees}
+        
+        return dfs(id)
