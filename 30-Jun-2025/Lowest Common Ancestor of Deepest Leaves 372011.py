d4a4a04a657f4ad8a909510fcdea0e70faf48bab
Ruth <165259669+eelut@users.noreply.github.com>
2025-06-30 14:42:27 +0300

    Created 'Lowest Common Ancestor of Deepest Leaves' by A2SV Hub

---
+++ 30-Jun-2025/Lowest Common Ancestor of Deepest Leaves 372011.py
@@ -0,0 +1,23 @@
+# Problem: Lowest Common Ancestor of Deepest Leaves - https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def helper(node):
+            if not node:
+                return (0, None)
+            l_depth, l_lca = helper(node.left)
+            r_depth, r_lca = helper(node.right)
+            if l_depth == r_depth:
+                return (l_depth + 1, node)
+            elif l_depth > r_depth:
+                return (l_depth + 1, l_lca)
+            else:
+                return (r_depth + 1, r_lca)
+
+        return helper(root)[1]
