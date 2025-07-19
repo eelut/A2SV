6556cc670ff60295590c5b351c0a0e1746b575e4
Ruth <165259669+eelut@users.noreply.github.com>
2025-07-19 15:11:24 +0300

    Created 'Non-overlapping Intervals' by A2SV Hub

---
+++ 19-Jul-2025/Non-overlapping Intervals 377917.py
@@ -0,0 +1,17 @@
+# Problem: Non-overlapping Intervals - https://leetcode.com/problems/non-overlapping-intervals/description/
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        prev=intervals[0][1]
+        res=0
+        for start,end in intervals[1:]:
+            if start >=prev:
+                prev=end
+            else:
+                res+=1
+                prev=min(prev,end)
+        return res
+
+
+        
