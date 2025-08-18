b883cd14f714f4ec9fe70680c89bc38aefcc0e92
Ruth <165259669+eelut@users.noreply.github.com>
2025-08-19 00:45:45 +0300

    Created 'D - Maxim’s Gift: Tangle of Letters' by A2SV Hub

---
+++ A2SV Contest #26 18-Aug-2025/D - Maxim’s Gift: Tangle of Letters 386708.py
@@ -0,0 +1,31 @@
+# Problem: D - Maxim’s Gift: Tangle of Letters - https://codeforces.com/gym/629689/problem/D
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+    res = len(s)
+    if n % 2 == 0:
+        v = [[0] * 26 for _ in range(2)]
+        for i in range(n):
+            v[i % 2][ord(s[i]) - ord('a')] += 1
+        for i in range(2):
+            mx = max(v[i])
+            res -= mx
+        print(res)
+    else:
+        pref = [[0] * 26 for _ in range(2)]
+        suf = [[0] * 26 for _ in range(2)]
+        for i in range(n - 1, -1, -1):
+            suf[i % 2][ord(s[i]) - ord('a')] += 1
+        for i in range(n):
+            suf[i % 2][ord(s[i]) - ord('a')] -= 1
+            ans = n
+            for k in range(2):
+                mx = 0
+                for j in range(26):
+                    mx = max(mx, suf[1 - k][j] + pref[k][j])
+                ans -= mx
+            res = min(res, ans)
+            pref[i % 2][ord(s[i]) - ord('a')] += 1
+        print(res)
