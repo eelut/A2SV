1fc04e3497a8e118a11facb4ab214421d52ed404
Ruth <165259669+eelut@users.noreply.github.com>
2025-08-25 22:13:24 +0300

    Created 'Hamming Distance' by A2SV Hub

---
+++ 25-Aug-2025/Hamming Distance 388876.py
@@ -0,0 +1,11 @@
+# Problem: Hamming Distance - https://leetcode.com/problems/hamming-distance/
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor=x^y
+        count=0
+        while xor:
+            count+=xor & 1
+            xor>>=1
+        return count
+        
