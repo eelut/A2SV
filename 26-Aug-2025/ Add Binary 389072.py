466bfc514f88532fa4ba43ab3f1a8bc8a50cad4e
Ruth <165259669+eelut@users.noreply.github.com>
2025-08-26 21:21:57 +0300

    Created ' Add Binary' by A2SV Hub

---
+++ 26-Aug-2025/ Add Binary 389072.py
@@ -0,0 +1,18 @@
+# Problem:  Add Binary - https://leetcode.com/problems/add-binary/
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        carry = 0
+        res = ""
+        a,b=a[::-1],b[::-1]
+        for i in range(max(len(a),len(b))):
+            digitA=ord(a[i])-ord("0") if i<len(a) else 0
+            digitB=ord(b[i])-ord("0") if i<len(b) else 0
+
+            total=digitA+digitB+carry
+            char=str(total%2)
+            res=char+res
+            carry=total//2
+        if carry:
+            res="1" +res
+        return res
