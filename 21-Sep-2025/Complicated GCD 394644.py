646abbf50dd0830c765d6a4b5606b4cc833c70a8
Ruth <165259669+eelut@users.noreply.github.com>
2025-09-21 17:06:10 +0300

    Created 'Complicated GCD' by A2SV Hub

---
+++ 21-Sep-2025/Complicated GCD 394644.py
@@ -0,0 +1,17 @@
+# Problem: Complicated GCD - https://codeforces.com/contest/664/problem/A
+
+import sys
+
+tokens = sys.stdin.read().strip().split()
+if not tokens:
+    sys.exit(0)
+
+a_raw, b_raw = tokens[0], tokens[1]
+
+a = a_raw.lstrip('0') or '0'
+b = b_raw.lstrip('0') or '0'
+
+if a == b:
+    print(a)
+else:
+    print(1)
