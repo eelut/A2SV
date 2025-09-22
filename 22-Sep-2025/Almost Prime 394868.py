ec15bd353122ab004859d7a2f84441d491582637
Ruth <165259669+eelut@users.noreply.github.com>
2025-09-22 10:47:31 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 22-Sep-2025/Almost Prime 394868.py
@@ -0,0 +1,19 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+def solve():
+    n = int(input())
+    
+    prime_factors_count = [0] * (n + 1)
+    
+    for i in range(2, n + 1):
+        if prime_factors_count[i] == 0:  
+            for j in range(i, n + 1, i):
+                prime_factors_count[j] += 1
+    
+    result = sum(1 for x in prime_factors_count if x == 2)
+    
+    print(result)
+
+
+if __name__ == "__main__":
+    solve()
