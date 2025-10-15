54abb775eddfb52cffbe3df88b435d02ae12bc9a
Ruth <165259669+eelut@users.noreply.github.com>
2025-10-15 21:36:31 +0300

    Created 'Count Cells in Overlapping Horizontal and Vertical Substrings' by A2SV Hub

---
+++ 15-Oct-2025/Count Cells in Overlapping Horizontal and Vertical Substrings 403525.py
@@ -0,0 +1,41 @@
+# Problem: Count Cells in Overlapping Horizontal and Vertical Substrings - https://leetcode.com/problems/count-cells-in-overlapping-horizontal-and-vertical-substrings/description/
+
+class Solution:
+    def countCells(self, grid: List[List[str]], pattern: str) -> int:
+        hor = ""
+        m = len(grid)
+        n = len(grid[0])
+        for arr in grid:
+            hor += "".join(arr)
+
+        res1 = []
+        x = len(pattern)
+        pr = 0
+        for i in range(len(hor)-len(pattern)+1):
+            if hor[i:i+x] == pattern:
+                res1.append((max(i,pr),i+x))
+                pr = i+x
+        if len(res1) == 0:
+            return 0
+        res = set()
+        for a,b in res1:
+            for i in range(a,b):
+                res.add((i//n,(i%n)))
+                
+        var = ""
+        for i in range(len(grid[0])):
+            for j in range(len(grid)):
+                var += grid[j][i]
+        res2 = set()
+        prev  =0 
+        for i in range(len(var)-x+1):
+            if var[i:i+x] == pattern:
+                for j in range(max(prev,i),i+x):
+                    res2.add((j%m,j//m))
+                prev = i+x
+                    
+        res = res.intersection(res2)
+       
+            
+        return len(res)
+        
