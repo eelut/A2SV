d74af2c7857a32ccfc014bf9720f269c49b96cee
Ruth <165259669+eelut@users.noreply.github.com>
2025-10-23 23:01:29 +0300

    Created 'Majority Element II (Optional)' by A2SV Hub

---
+++ 23-Oct-2025/Majority Element II (Optional) 405407.py
@@ -0,0 +1,14 @@
+# Problem: Majority Element II (Optional) - https://leetcode.com/problems/majority-element-ii/
+
+class Solution:
+    def majorityElement(self, nums: list[int]) -> list[int]:
+        element_count = Counter(nums)
+        
+        majority_elements = []
+        threshold = len(nums) // 3
+        
+        for element, count in element_count.items():
+            if count > threshold:
+                majority_elements.append(element)
+        
+        return majority_elements
